e601dc63bd36a634c8fd81b68296b044945bc27a
chiragvispute <chiragvispute30@gmail.com>
2025-08-24 19:21:52 +0530

    supabase database

---
+++ nabard/lib/config/supabase_config.dart
@@ -0,0 +1,5 @@
+class SupabaseConfig {
+  static const String url = 'https://tasxfstqvegddfoljxxz.supabase.co';
+  static const String anonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhc3hmc3RxdmVnZGRmb2xqeHh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NzkzMTQsImV4cCI6MjA3MTU1NTMxNH0.sKONffOMQ12SKGUIGH2UA7Iq6hQMNp_Flog7oMj368Q';
+}
